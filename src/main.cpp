c769b364ba08443549f95d760cca05e8916b127a
ReelBadMan <ciecuits@gmail.com>
2024-08-31 15:40:52 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,75 @@
 #include <iostream>
+#include <unordered_map>
+#include <string>
+
+class Shell {
+public:
+    Shell() {
+        commandMap["echo"] = "echo is a shell builtin";
+        commandMap["exit"] = "exit is a shell builtin";
+        commandMap["type"] = "type is a shell builtin";
+    }
+
+    void run() {
+        while(true) {
+            std::cout<<"$ ";
+
+            std::string input;
+            std::getline(std::cin, input);
+
+            if(input.empty()) {
+                break;
+            }
+
+
+            std::string command = parseCommand(input);
+
+            // exit built it
+            if (command == "exit") {
+                exit(0);
+            }
+
+            if(command=="echo") {
+                echoCommand(input);
+            }else if(command=="type") {
+                typeCommand(input.substr(5));
+            }else {
+                std::cerr << command << ": command not found\n";
+            }
+        }
+    }
+
+private:
+
+    std::unordered_map<std::string, std::string> commandMap;
+
+    std::string parseCommand(const std::string& input) {
+        return input.substr(0, input.find(' '));
+    }
+
+    void echoCommand(const std::string& input) {
+        std::cout << input.substr(5) << std::endl;
+    }
+
+    void typeCommand(const std::string& command) {
+        // Check if the command exists in the map and output its type
+
+        if(commandMap.find(command) != commandMap.end()) {
+            std::cout << commandMap[command] << std::endl;
+        }else {
+            std:: cout << command << ": not found" << std::endl;
+        }
+    }
+};
+
+
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+  Shell shell;
+    shell.run();
 
-  // Uncomment this block to pass the first stage
-
-    while(true) {
-        std::cout << "$ ";
-
-        std::string input;
-        std::getline(std::cin, input);
-
-        if(input.empty()) {
-            break;
-        }
-    // exit built it
-        if (input == "exit 0") {
-            exit(0);
-        }
-
-    //echo built in
-
-        if(input.substr(0,4)=="echo") {
-            std::cout<<input.substr(5)<<std::endl;
-        }else {
-            std::cerr <<input << ": command not found\n";
-        }
-
-    }
     return 0;
 }
